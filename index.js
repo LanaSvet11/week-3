985a428c05738d48ae2daa95495022730115ef0d
Svitlana Holovatenko <sv.golovatenko@gmail.com>
2023-08-14 13:53:06 -0700

    new material

---
+++ index.js
@@ -0,0 +1,35 @@
+for (let i=0; i<11; i++) {
+    if (i < 5) {
+        console.log(i)
+    }
+}
+
+console.log ("my name is Chase and one of my favority numeber is", 60);
+console.log("my name is Chase and one of my favority numeber is", 60);
+console.log("my name is Chase and one of my favority numeber is", 60);
+console.log("my name is Chase and one of my favority numeber is", 60);
+console.log("my name is Chase and one of my favority numeber is", 60);
+
+for (let i=60; i<70; i++) {
+    if (i==60 i==61 i==62)
+}
+
+
+///////////
+const returnEmailIsValid = (email) => {
+    if (email.length <10 || email.length > 50 || !email.includes('@')) {
+        return false;
+    } else {
+        return true;
+    }
+}
+const emailIsValid = returnEmailIsValid("hello@mysite.com");
+if (emailIsValid) {
+    console.log("welcome to the site!");
+} else {
+    console.log("please use a good email");
+}
+
+//////////////////////
+
+
